a1e3f0ab12734fdc2f941dda5dc3fd044dfb2578
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-02-16 13:22:41 +0100

    Add more spec

--- spec/problems/algorithms/algorithms_data_structures_spec.rb
+++ spec/problems/algorithms/algorithms_data_structures_spec.rb
@@ -15,9 +15,15 @@ RSpec.describe 'Problems::Algorithms' do
   end
 
   describe 'valid inputs' do
-    context 'when inputs values can complete the circuit' do
+    context 'when inputs values can complete the circuit: 1' do
       it 'returns 3' do
         expect(algo.can_complete_circuit([1,2,3,4,5], [3,4,5,1,2])).to eq(3)          
+      end
+    end
+
+    context 'when inputs values can complete the circuit: 2' do
+      it 'returns 4' do
+        expect(algo.can_complete_circuit([5,1,2,3,4], [4,4,1,5,1])).to eq(4)          
       end
     end
 
