2e95b2becefe03e1373c4036a005978beeb8c75c
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-12-27 12:26:09 +0200

    Implement two_sum using index instead of the actual value

--- problems/algorithms/two_sum.rb
+++ problems/algorithms/two_sum.rb
@@ -1,18 +1,31 @@
 
+# def two_sum(array, target)
+#   hash = {}
+#   array.each do |arr|
+#     if hash[target - arr]
+#       return [hash[target - arr], array.index(arr)]
+#     else
+#       hash[arr] = array.index(arr)
+#     end
+#   end
+#   nil
+# end
+
 def two_sum(array, target)
   hash = {}
-  array.each do |arr|
-    if hash[target - arr]
-      return [hash[target - arr], array.index(arr)]
+
+  (0...array.size).each do |i|
+    if hash[target - array[i]]
+      return [hash[target - array[i]], i]
     else
-      hash[arr] = array.index(arr)
+      hash[array[i]] = i
     end
   end
   nil
 end
 
 arr1 = [11, 2, 7, 15]
-p two_sum(arr1, 9)
+p two_sum(arr1, 26)
 
 arr2 = [3, 2, 5, 9, 4]
 p two_sum(arr2, 6)
