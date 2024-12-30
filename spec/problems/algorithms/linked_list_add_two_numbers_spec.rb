8869de237caa395d460186b055bae3131526a765
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-12-30 14:43:42 +0200

    Add new test

--- spec/problems/algorithms/linked_list_add_two_numbers_spec.rb
+++ spec/problems/algorithms/linked_list_add_two_numbers_spec.rb
@@ -4,22 +4,30 @@ require_relative '../../../problems/algorithms/linked_list_add_two_numbers.rb'
 RSpec.describe 'Problems::Algorithms' do
   let(:algo) { Class.new { extend Problems::Algorithms } }
 
-  let!(:node_list_one) do
-  end
+  let!(:node_list_one) { Problems::Algorithms::ListNode.populate_from_array([2,4,3]) }
 
-  let!(:node_list_two) do
-  end
+  let!(:node_list_two) { Problems::Algorithms::ListNode.populate_from_array([5,6,4]) }
 
   describe 'Invalid inputs' do
-    context 'when linked_lists are negative values' do
+    context 'when linked_lists contains negative values' do
+      let(:list_one) { Problems::Algorithms::ListNode.populate_from_array([-3]) }
+      let(:list_two) { Problems::Algorithms::ListNode.populate_from_array([5]) }
+
       it 'returns nil' do
-        expect(algo.add_two_numbers(node_list_one, node_list_two)).to be_nil
+        expect(algo.add_two_numbers(list_one, list_two)).to be_nil
       end
     end
   end
 
   describe 'valid inputs' do
-    context 'when both linked_lists input values are zero' do
+
+    context 'when both linked_lists input value are zeros' do
+      let(:list_one) { Problems::Algorithms::ListNode.populate_from_array([0]) }
+      let(:list_two) { Problems::Algorithms::ListNode.populate_from_array([0]) }
+
+      it 'returns a node with zero value' do
+        expect(algo.add_two_numbers(list_one, list_two).val).to eq 0
+      end
     end
 
     context '' do
